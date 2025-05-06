4827c06d9c8675045df968ec2e342e50c43fbc76
Taehyun Kwon <ktaehyun@amazon.com>
2025-05-05 18:53:46 -0700

    Update submit.sh with our team member names

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Taehyun Kwon, kwontae\nVictor Rojo, vicrojo\nThangakumar Dhanasekaran, tdhana" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
